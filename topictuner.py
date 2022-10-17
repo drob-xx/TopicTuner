e8a0f0c284448a1fdfe5cb518c8163f0125b81e4
DanR <dan@drob.org>
2022-10-17 19:40:48 +0000

    fix

--- topictuner.py
+++ topictuner.py
@@ -333,7 +333,7 @@ class TopicModelTuner(object):
     def simpleSearch(self, cluster_sizes, sample_sizes) :
       if len(cluster_sizes) != len(sample_sizes) :
         raise ValueError('Length of cluster sizes and samples sizes lists must match')
-      return _runTests([self.paramPair(cs,ss) for cs, ss in zip(cluster_size, sample_sizes)])
+      return self._runTests([self.paramPair(cs,ss) for cs, ss in zip(cluster_size, sample_sizes)])
   
     def visualizeSearch(self, resultsDF) :
       '''
