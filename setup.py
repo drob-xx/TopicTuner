763333f48edead75caa7043345996b23ef1bb763
DanR <dan@drob.org>
2023-01-10 19:36:26 -0800

    setting up pip

---
+++ setup.py
@@ -0,0 +1,35 @@
+import setuptools
+import re
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+VERSIONFILE="topictuner/__init__.py"
+getversion = re.search( r"^__version__ = ['\"]([^'\"]*)['\"]", open(VERSIONFILE, "rt").read(), re.M)
+if getversion:
+    new_version = getversion.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+
+
+setuptools.setup(
+     install_requires=['bertopic'],        # Dependencies
+     python_requires='>=3',                                   # Minimum Python version
+     name='topicmodeltuner',                                  # Package name
+     version=new_version,                                             # Version
+     author="Dan Robinson",                                     # Author name
+     author_email="",                           # Author mail
+     description="HDBSCAN Tuning for BERTopic Models",    # Short package description
+     long_description=long_description,                       # Long package description
+     long_description_content_type="text/markdown",
+     url="https://github.com/drob-xx/TopicTuner",       # Url to your Git Repo
+     download_url = 'https://github.com/drob-xx/TopicTuner/archive/refs/tags/'+new_version+'.tar.gz',
+     packages=setuptools.find_packages(),                     # Searches throughout all dirs for files to include
+     include_package_data=True,                               # Must be true to include files depicted in MANIFEST.in
+     license_files=["LICENSE"],                               # License file
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: GNU GPL",
+         "Operating System :: OS Independent",
+     ],
+ )
